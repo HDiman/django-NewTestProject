9e6698ed24ca62820dc920823d0cf08fab189f39
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-09-13 21:31:38 +0300

    Yield

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import random
+
 print("Starting to code Galactic Finance Game")
 
+
+def func_itr():
+    while True:
+        yield random.random()
+
+
+rnd_num = func_itr()
+print(rnd_num.__next__())
+print(rnd_num.__next__())
+print(rnd_num.__next__())
 
