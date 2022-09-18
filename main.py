a29c85e421419f454448dc1dfb382fa073d41c79
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-09-18 10:14:13 +0300

    Yield + Lambda + Decorator + Enumerate

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-import random
+# import random
+#
+# print("Starting to code Galactic Finance Game")
+#
+#
+# def func_itr():
+#     while True:
+#         yield int(random.random() * 100)
+#
+#
+# rnd_num = func_itr()
+# print(rnd_num.__next__())
+# print(rnd_num.__next__())
+# print(rnd_num.__next__())
+#
+#
+# lst = [1, 2, 3, 4, 5]
+# new_list = list(map(lambda x: x*2, lst))
+# print(new_list)
+#
+#
+#
+# def make_important(fn):
+#     def wrapped():
+#         return "!" + fn() + "!"
+#     return wrapped
+#
+# @make_important
+# def hello():
+#     return 'hello habr'
+#
+# print(hello())
 
-print("Starting to code Galactic Finance Game")
-
-
-def func_itr():
-    while True:
-        yield int(random.random() * 100)
-
-
-rnd_num = func_itr()
-print(rnd_num.__next__())
-print(rnd_num.__next__())
-print(rnd_num.__next__())
-
-
-lst = [1, 2, 3, 4, 5]
-new_list = list(map(lambda x: x*2, lst))
-print(new_list)
-
-
-
-def make_important(fn):
-    def wrapped():
-        return "!" + fn() + "!"
-    return wrapped
-
-@make_important
-def hello():
-    return 'hello habr'
-
-print(hello())
+lst = ["first", "second", "third"]
+for index, value in enumerate(lst):
+    print(index, value)
